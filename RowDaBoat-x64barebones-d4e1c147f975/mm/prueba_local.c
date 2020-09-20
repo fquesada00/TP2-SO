c0d3bf01b83845ce55847595ae3ac91515261b33
oserpe <oserpe@itba.edu.ar>
2020-09-20 14:20:53 -0300

    memory manager terminado y funcional (arreglar comentarios y ctes)

--- RowDaBoat-x64barebones-d4e1c147f975/mm/prueba_local.c
+++ RowDaBoat-x64barebones-d4e1c147f975/mm/prueba_local.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define CTE (size_t)1000000 * 4      //store 4mb MODIFICAR ESTO
+#define CTE (size_t)1024 * 1024 * 4  //store 4mb MODIFICAR ESTO
 #define MINIMUN_BLOCK_SIZE (size_t)8 //8 bytes
 
 #define WORD_ALIGN 8 //Buscamos en el manual del Pure y usa 8 bytes https://tracker.pureos.net/w/pureos/hardware_requirements/
@@ -53,31 +53,42 @@ void printDirections(char *p, int dim)
 {
     for (int i = 0; i < dim; i++)
     {
-        printf("Dir at pos %d: %p - ", i,p + i);
+        printf(" - Pos: %d at dir: %p - ", i, p + i);
     }
     printf("\n");
 }
 int main(void)
 {
-    //reserve 10mb
-    a_block a,b;
-    a.free=0;
-    b.free=1;
-    //printf("%d\n",heapHeaderSize);
-    //printf("%d %d\n",(sizeof(a) + (size_t)(WORD_ALIGN - 1)) & ~(WORD_ALIGN_MASK),(sizeof(b) + (size_t)(WORD_ALIGN - 1)) & ~(WORD_ALIGN_MASK));
+    //reserve 4mb
+    char *baseAddress = malloc(1024 * 1024 * 4); //(void*)0x7efec7045010
 
+    pInitHeap(baseAddress, baseAddress + (1024 * 1024 * 4 - 1));
 
-    char *baseAddress = malloc(1024 * 1024 * 10); //(void*)0x7efec7045010
-
-    pInitHeap(baseAddress, baseAddress + (1024 * 1024 * 10 - 1));
-
-    char *temp = pMalloc(sizeof(int) * 4);
-    printDirections(temp, 4);
-    printf("temp-header: %p & %p\n",(temp-heapHeaderSize), temp);
+    char *temp = pMalloc(sizeof(char) * 4);
     pFree(temp);
-    
-    temp = pMalloc(sizeof(int) * 4);
-    printDirections(temp, 5);
+
+    temp = pMalloc(sizeof(char) * 2);
+    printDirections(temp, 2);
+    char *p = pMalloc(sizeof(char) * 4);
+    printDirections(p, 4);
+    char *k = pMalloc(sizeof(char) * 2);
+    printDirections(k, 2);
+    pFree(p);
+    p = pMalloc(sizeof(char) * 9);
+    k = pMalloc(9);
+
+    p[0]='a';
+    p[1]='b';
+    p[2]='c';
+    p[3]='d';
+    k[0]='e';
+    k[1]='f';
+    for(int i = 0 ; i < 4 ; i++){
+        printf("%c",p[i]);
+    }
+    for(int i = 0 ; i < 2 ; i++){
+        printf("%c",k[i]);
+    }
 }
 
 void *pMalloc(size_t requestedSize)
@@ -122,14 +133,14 @@ void *pMalloc(size_t requestedSize)
             requestedSize += WORD_ALIGN;
             requestedSize &= ~WORD_ALIGN_MASK;
         }
-        printf("requested %d\n",requestedSize);
+
         /*
             Initialize search blocks
         */
         a_block *pPrevBlock, *pActualBlock;
         pPrevBlock = &heapStart;
         pActualBlock = pPrevBlock->pNextBlock;
-
+        printf("Actual: %p, requested: %ld\n", pActualBlock, requestedSize);
         /*
             Lookup doing first fit algorithm, tambien me fijo si esta libre
         */
@@ -138,23 +149,21 @@ void *pMalloc(size_t requestedSize)
             pPrevBlock = pActualBlock;
             pActualBlock = pActualBlock->pNextBlock;
         }
+        printf("Actual post while: %p\n", pActualBlock);
 
         if (pActualBlock->pNextBlock == NULL)
         {
-
+            return NULL;
         } //throw exception not enough space (reached end)
 
         /*
             Set block to be returned without header as well
         */
-        pReturnBlock = (void *)pActualBlock + heapHeaderSize;
+        pReturnBlock = (void *)((u_int8_t *)pActualBlock + heapHeaderSize);
 
         /*
             Skip block pointer as its going to be occupied 
         */
-        //Bueno, aca tenemos una diferencia de implementacion, aca lo que hace es ignora los bloques que no estan libres,
-        //osea tiene una lista de libres y los ocupados los deja por ahi, cuando se liberan los vuelve a incorporar
-        //Como es mas eficiente la que implementan ellos, la dejo, sino tendriamos que checkear el free en elwhile de arriba y cambiarlo
         pPrevBlock->pNextBlock = pActualBlock->pNextBlock;
 
         /*
@@ -163,17 +172,19 @@ void *pMalloc(size_t requestedSize)
             as its mandatory to allocate memory).
             If not, then do not link, oppositly, link it
         */
-        if ((pActualBlock->BlockSize - requestedSize) > (remainingBytes - heapHeaderSize))
+        printf("%p y %ld\n", pActualBlock, pActualBlock->BlockSize);
+        printf("%ld <= %ld\n", pActualBlock->BlockSize - requestedSize, remainingBytes);
+        if ((pActualBlock->BlockSize - requestedSize) <= (remainingBytes))
         {
-            a_block *pLinkBlock = pActualBlock + requestedSize;
+            a_block *pLinkBlock = (a_block *)((u_int8_t *)pActualBlock + requestedSize);
             pLinkBlock->BlockSize = pActualBlock->BlockSize - requestedSize;
             pLinkBlock->free = 1; //Esto no es necesario porque este seria el libre, va, si 1 es ocupado, ademas como esta implementado no es necesario
-
+            //pPrevBlock->pNextBlock = pLinkBlock;
             /*
                 Update new size as its less than before
             */
-            pActualBlock->BlockSize = requestedSize; //No nos faltaria sumar el heap size, porque los bloques tienen el tamaño del heap
-            //cuando tratemos de liberar vamos a tener tamaño equivocado
+            pActualBlock->BlockSize = requestedSize;
+            printf("link: %p\n", pLinkBlock);
             pInsertBlockIntoList(pLinkBlock);
         }
 
@@ -185,7 +196,8 @@ void *pMalloc(size_t requestedSize)
         pActualBlock->pNextBlock = NULL;
         pActualBlock->free = 0;
     }
-    printf("return %p\n",pReturnBlock);
+    printf("return %p\n", pReturnBlock);
+    printf("------\n\n");
     return pReturnBlock;
 }
 
@@ -194,7 +206,6 @@ void *pMalloc(size_t requestedSize)
 */
 void pInitHeap(void *baseAddress, void *endAddress)
 {
-
     /*
         Set start struct allocated in stack
     */
@@ -212,7 +223,6 @@ void pInitHeap(void *baseAddress, void *endAddress)
     pFirstBlock->pNextBlock = heapEnd;
     pFirstBlock->BlockSize = (size_t)heapEnd - (size_t)pFirstBlock;
     pFirstBlock->free = 1;
-    //printf("first %p, %d\n",pFirstBlock,(sizeof(*pFirstBlock) + (size_t)(WORD_ALIGN - 1)) & ~(WORD_ALIGN_MASK));
 
     remainingBytes = pFirstBlock->BlockSize;
 }
@@ -222,37 +232,39 @@ void pInitHeap(void *baseAddress, void *endAddress)
 */
 void pFree(void *pointer)
 {
-
-    size_t *pNextABlock = (size_t *)pointer;
+    /*
+        Cast pointer as we are going to move 1 byte per address
+    */
+    printf("%p\n", pointer);
+    u_int8_t *pNextABlock = (u_int8_t *)pointer;
     a_block *pLinkBlock;
-    printf("%p\n",pNextABlock);
 
     if (pNextABlock == NULL)
     {
-
+        return;
     } //nothing to do here
-    
+
     /*
         Before the pointer there is a_block struct
     */
     pNextABlock -= heapHeaderSize;
-    printf("%d\n",heapHeaderSize);
-    printf("nextBlock: %p\n",pNextABlock);
     pLinkBlock = (a_block *)pNextABlock;
-    printf("%p\n", pLinkBlock);
-    //ACA ESTA FALLANDO
-    if (pLinkBlock->free == 0 || pLinkBlock->pNextBlock == NULL)
+
+    if (pLinkBlock->pNextBlock != NULL)
     {
-
+        return;
     } //we did something wrong so
-    h();
-    remainingBytes += pLinkBlock->BlockSize;
 
+    remainingBytes += pLinkBlock->BlockSize;
+    
+        printf("FREE\n");
+    printf("FREE\n");
     pInsertBlockIntoList(pLinkBlock);
 }
 
 void pInsertBlockIntoList(a_block *pInsertBlock)
 {
+    printf("insert: %p\n", pInsertBlock);
     a_block *pPrevBlock = &heapStart;
 
     /*
@@ -261,20 +273,23 @@ void pInsertBlockIntoList(a_block *pInsertBlock)
     */
     while (pPrevBlock->pNextBlock < pInsertBlock)
     {
+        printf("1");
         pPrevBlock = pPrevBlock->pNextBlock;
     }
+    printf("%p & %p\n", pPrevBlock->pNextBlock, pPrevBlock);
 
-    if (pPrevBlock->free == 0 || pPrevBlock->pNextBlock == NULL)
+    if (pPrevBlock->pNextBlock == NULL)
     {
-
+        return;
     } //we did something wrong
 
     /*
         Check if the previous block to the one being inserted make
         a big block that is next to each other, if yes, then merge
     */
-    if ((pPrevBlock + pPrevBlock->BlockSize) == pInsertBlock)
+    if ((a_block *)((u_int8_t *)pPrevBlock + pPrevBlock->BlockSize) == pInsertBlock)
     {
+        printf("1");
         pPrevBlock->BlockSize += pInsertBlock->BlockSize;
         pInsertBlock = pPrevBlock;
     }
@@ -284,8 +299,9 @@ void pInsertBlockIntoList(a_block *pInsertBlock)
         If a previous merge was done, it doesnt modify
         anything as we are dealing with pointers
     */
-    if ((pInsertBlock + pInsertBlock->BlockSize) == pPrevBlock->pNextBlock)
+    if ((a_block *)((u_int8_t *)pInsertBlock + pInsertBlock->BlockSize) == pPrevBlock->pNextBlock)
     {
+        printf("2");
         /*
             If end was reached, then dont increase size.
             Else, skip intermediate block
@@ -313,4 +329,6 @@ void pInsertBlockIntoList(a_block *pInsertBlock)
     {
         pPrevBlock->pNextBlock = pInsertBlock;
     }
+
+    printf("Prev: %p, Insert: %p, Next: %p\n", pPrevBlock->pNextBlock, pInsertBlock, pInsertBlock->pNextBlock);
 }
