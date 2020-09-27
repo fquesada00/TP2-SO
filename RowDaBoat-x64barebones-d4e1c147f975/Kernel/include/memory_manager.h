e04dd6d833769f6a6d6579b264f516cb3e350688
oserpe <oserpe@itba.edu.ar>
2020-09-27 12:44:27 -0300

    memory manager done and modularized

--- RowDaBoat-x64barebones-d4e1c147f975/Kernel/include/memory_manager.h
+++ RowDaBoat-x64barebones-d4e1c147f975/Kernel/include/memory_manager.h
@@ -3,8 +3,9 @@
 #include <stdint.h>
 #include <stdlib.h>
 
-#define CTE (size_t)1000000 * 4      //store 4mb MODIFICAR ESTO
-#define MINIMUN_BLOCK_SIZE (size_t)8 //8 bytes
+#define BASE_ADDRESS 0x800000
+#define END_ADDRESS 0xC00000
+#define HEAP_SIZE (size_t) BASE_ADDRESS - (size_t)END_ADDRESS   
 #define WORD_ALIGN 8 
 #define WORD_ALIGN_MASK 7
 
@@ -14,9 +15,24 @@ typedef struct a_block
     size_t BlockSize;
 } a_block;
 
+/*
+    Reserve requestedSize bytes in heap
+*/
 void *pMalloc(size_t);
+
+/*
+    Here baseAddress & endAddress must be word aligned
+*/
 void pInitHeap(void *, void *);
+
+/*
+    Receive a pointer to be freed
+*/
 void pFree(void *);
+
+/*
+    Receive a block to be inserted into the free list
+*/
 void pInsertBlockIntoList(a_block *);
 
 #endif
--- RowDaBoat-x64barebones-d4e1c147f975/Kernel/mm/memory_manager.c
+++ RowDaBoat-x64barebones-d4e1c147f975/Kernel/mm/memory_manager.c
@@ -1,182 +1,239 @@
-// #include "memory_manager.h"
+#include "../include/memory_manager.h"
 
-// //chequear mascara y alineamiento
-// static const size_t heapHeaderSize = sizeof(a_block) & (size_t)-2;
-// static size_t remainingBytes = CTE;
-// static a_block heapStart, *heapEnd = NULL;
+/*
+    Knowing the size of the header by bitwise operations 
+    and word alignment to set it to a multiply of 8, we
+    can move through the heap efficiently
 
-// /*
-//     LINKS DE INTERES
-//     https://courses.cs.washington.edu/courses/cse351/10sp/lectures/15-memallocation.pdf
-//     https://www.youtube.com/watch?v=74s0m4YoHgM
-//     https://github.com/FreeRTOS/FreeRTOS-Kernel/blob/master/portable/MemMang/heap_4.c
-//     using implicit free list w/ first fit algorithm 
-// */
+    Alternativa ineficiente en operaciones nivel procesador:
+        heapHeaderSize = sizeof(a_block) + ((WORD_ALIGN - sizeof(a_block)%WORD_ALIGN))%WORD_ALIGN
+*/
+static const size_t heapHeaderSize = (sizeof(a_block) + (size_t)(WORD_ALIGN - 1)) & ~(WORD_ALIGN_MASK);
 
+static size_t remainingBytes = HEAP_SIZE;
 
-// void *pMalloc(size_t requestedSize)
-// {
+/*
+    With a struct in stack and a struct pointer to the end
+    of the heap, we can move through it
+*/
+static a_block heapStart, *heapEnd = NULL;
 
-//     //if heap not initialize, do it
-//     if (heapEnd == NULL)
-//     {
-//         //MODIFICAR ESTO CON CTES ESTARIA COPADO
-//         pInitHeap(0x800000, 0xC000000);
-//     }
-    
-//         if (remainingBytes < requestedSize)
-//         {
+/*
+    LINKS DE INTERES
+    https://courses.cs.washington.edu/courses/cse351/10sp/lectures/15-memallocation.pdf
+    https://www.youtube.com/watch?v=74s0m4YoHgM
+    https://github.com/FreeRTOS/FreeRTOS-Kernel/blob/master/portable/MemMang/heap_4.c
+    using implicit free list with first fit algorithm 
+*/
 
-//         } //throw exception not enough space
-//         if (requestedSize < 0)
-//         {
+void *pMalloc(size_t requestedSize)
+{
+    void *pReturnBlock = NULL;
 
-//         } //throw exception negative sizes not accepted
+    /*
+        If heap is not initialized, then do it
+    */
+    if (heapEnd == NULL)
+    {
+        pInitHeap(BASE_ADDRESS, END_ADDRESS);
+    }
 
-//         //passed controls then go on w/ first fit
-//         a_block *pPrevBlock, *pActualBlock;
-//         void *pReturnBlock;
+    if (remainingBytes < requestedSize)
+    {
 
-//         pPrevBlock = &heapStart;
-//         pActualBlock = pPrevBlock->pNextBlock;
+    } //throw exception not enough space
+    if (requestedSize < 0)
+    {
 
-//         //find available block
-//         while (pActualBlock->BlockSize < requestedSize && pActualBlock->pNextBlock != NULL)
-//         {
-//             pPrevBlock = pActualBlock;
-//             pActualBlock = pActualBlock->pNextBlock;
-//         }
+    } //throw exception negative sizes not accepted
 
-//         if (pActualBlock->pNextBlock == NULL)
-//         {
+    /*
+            Header struct is part of the size
+    */
+    requestedSize += heapHeaderSize;
 
-//         } //throw exception not enough space (reached end)
+    /*
+        To align, check if requestedSize is not multiple of WORD_ALIGN_MASK.
+        If yes, then its done.
+        If not, then add WORD_ALIGN and bitwise "and" operator with
+        "not" WORD_ALIGN_MASK to set last bits multiple of WORD_ALIGN
 
-//         pReturnBlock = heapHeaderSize + (void *)pActualBlock;
+        Alternativa ineficiente en operaciones nivel procesador:
+            requestedSize += (WORD_ALIGN - requestedSize%WORD_ALIGN)%WORD_ALIGN
+    */
+    if ((requestedSize & WORD_ALIGN_MASK) != 0)
+    {
+        requestedSize += WORD_ALIGN;
+        requestedSize &= ~WORD_ALIGN_MASK;
+    }
 
-//         //block must be taken out as its going to be used
-//         pPrevBlock->pNextBlock = pActualBlock->pNextBlock;
+    /*
+        Initialize search blocks
+    */
+    a_block *pPrevBlock, *pActualBlock;
+    pPrevBlock = &heapStart;
+    pActualBlock = pPrevBlock->pNextBlock;
 
-//         //partition to use more than 1 block
-//         //TODO
-//         if ((pActualBlock->BlockSize - requestedSize) > MINIMUN_BLOCK_SIZE)
-//         {
+    /*
+        Lookup doing first fit algorithm
+    */
+    while (pActualBlock->BlockSize < requestedSize && pActualBlock->pNextBlock != NULL)
+    {
+        pPrevBlock = pActualBlock;
+        pActualBlock = pActualBlock->pNextBlock;
+    }
 
-//             a_block *pPartitionedBlock = NULL;
-//             insertBlockIntoList(pPartitionedBlock);
-//         }
+    if (pActualBlock->pNextBlock == NULL)
+    {
+        return NULL;
+    } //throw exception not enough space (reached end)
 
-//         //supongo que salio todo ok
-//         remainingBytes -= pActualBlock->BlockSize;
-//         pActualBlock->pNextBlock = NULL;
-    
-// }
+    /*
+        Set block to be returned without header as well
+    */
+    pReturnBlock = (void *)((uint8_t *)pActualBlock + heapHeaderSize);
 
-// //baseAddress & endAddress must be 8 byte aligned
-// void pInitHeap(void *baseAddress, void *endAddress)
-// {
+    /*
+        Skip block pointer as its going to be occupied 
+    */
+    pPrevBlock->pNextBlock = pActualBlock->pNextBlock;
 
-//     //set start header static value
-//     heapStart.pNextBlock = (a_block *)baseAddress;
-//     heapStart.BlockSize = (size_t)0;
+    /*
+        Now memory must be partitioned as its free space available
+        to storage at least one byte more (not counting a_block struct
+        as its mandatory to allocate memory).
+        If not, then do not link, oppositly, link it
+    */
+    if ((pActualBlock->BlockSize - requestedSize) <= (remainingBytes))
+    {
+        a_block *pLinkBlock = (a_block *)((uint8_t *)pActualBlock + requestedSize);
+        pLinkBlock->BlockSize = pActualBlock->BlockSize - requestedSize;
 
-//     //set end having set start
-//     heapEnd = endAddress - heapHeaderSize;
-//     heapEnd->pNextBlock = NULL;
-//     heapEnd->BlockSize = (size_t)0;
+        /*
+            Update new size as its less than before
+        */
+        pActualBlock->BlockSize = requestedSize;
+        pInsertBlockIntoList(pLinkBlock);
+    }
 
-//     /* initialize first block
-//         his size is the diff where end begins and firstBlock + is size starts
-//     */
-//     a_block *pFirstBlock = (a_block *)baseAddress;
-//     pFirstBlock->pNextBlock = heapEnd;
-//     pFirstBlock->BlockSize = (size_t)heapEnd - (size_t)pFirstBlock;
+    remainingBytes -= pActualBlock->BlockSize;
 
-//     remainingBytes = pFirstBlock->BlockSize;
-// }
+    /*
+        Take out the block from the list
+    */
+    pActualBlock->pNextBlock = NULL;
 
-// void pFree(void *pointer)
-// {
+    return pReturnBlock;
+}
 
-//     size_t *pNextABlock = (size_t *)pointer;
-//     a_block *pToLink;
+void pInitHeap(void *baseAddress, void *endAddress)
+{
+    /*
+        Set start struct allocated in stack
+    */
+    heapStart.pNextBlock = (a_block *)baseAddress;
+    heapStart.BlockSize = (size_t)0;
 
+    heapEnd = endAddress - heapHeaderSize;
+    heapEnd->pNextBlock = NULL;
+    heapEnd->BlockSize = (size_t)0;
 
-//     if (pNextABlock == NULL)
-//     {
+    a_block *pFirstBlock = (a_block *)baseAddress;
+    pFirstBlock->pNextBlock = heapEnd;
+    pFirstBlock->BlockSize = (size_t)heapEnd - (size_t)pFirstBlock;
 
-//     } //nothing to do here
+    remainingBytes = pFirstBlock->BlockSize;
+}
 
-//     //before pointer its an struct
-//     pNextABlock -= heapHeaderSize;
-//     pToLink = (a_block *)pNextABlock;
+void pFree(void *pointer)
+{
+    /*
+        Cast pointer as we are going to move 1 byte per address
+    */
+    uint8_t *pNextABlock = (uint8_t *)pointer;
+    a_block *pLinkBlock;
 
-//     if (pToLink->pNextBlock != NULL)
-//     {
+    if (pNextABlock == NULL)
+    {
+        return;
+    } //nothing to do here
 
-//     } //we did something wrong so
+    /*
+        Before the pointer there is a_block struct
+    */
+    pNextABlock -= heapHeaderSize;
+    pLinkBlock = (a_block *)pNextABlock;
 
-//     remainingBytes += pToLink->BlockSize;
+    if (pLinkBlock->pNextBlock != NULL)
+    {
+        return;
+    } //we did something wrong so
 
-//     insertBlockIntoList(pToLink);
-    
-// }
+    remainingBytes += pLinkBlock->BlockSize;
 
-// void insertBlockIntoList(a_block *pInsertBlock)
-// {
+    pInsertBlockIntoList(pLinkBlock);
+}
 
-//     //iterate through the list
-//     a_block *pPrevBlock = &heapStart;
+void pInsertBlockIntoList(a_block *pInsertBlock)
+{
+    a_block *pPrevBlock = &heapStart;
 
-//     //never reaches end as we are asking for next block and inequality
-//     while (pPrevBlock->pNextBlock < pInsertBlock)
-//     {
-//         pPrevBlock = pPrevBlock->pNextBlock;
-//     }
+    /*
+        Starting from first block, we iterate along the list
+        until we find a block with a higher address than pInsertBlock
+    */
+    while (pPrevBlock->pNextBlock < pInsertBlock)
+    {
+        pPrevBlock = pPrevBlock->pNextBlock;
+    }
 
-//     //check if nextBlock of prevBlock is next to insertBlock, if so merge them
-//     if ((pPrevBlock + pPrevBlock->BlockSize) == pInsertBlock)
-//     {
-//         pPrevBlock->BlockSize += pInsertBlock->BlockSize;
-//         pInsertBlock = pPrevBlock;
-//     }
-//     else
-//     {
-//         //no entendi el error que habria q tirar aca 
-//     }
+    if (pPrevBlock->pNextBlock == NULL)
+    {
+        return;
+    } //we did something wrong
 
-//     //analogous but with next block from insertBlock, if so merge them
-//     //we do not care if a previous merge was done as we increase the size
-//     if ((pInsertBlock + pInsertBlock->BlockSize) == pPrevBlock->pNextBlock)
-//     {
-//         //if we reached the end then do not increase size
-//         if (pPrevBlock->pNextBlock == heapEnd){
-//             pInsertBlock->pNextBlock = heapEnd;
-//         }
-//         else
-//         {
-//             //set pointers skiping intermediate block
-//             //as said before if a previous merge was done does not change anything
-//             //as we are dealing with memory addresses
-//             pInsertBlock->BlockSize += pPrevBlock->pNextBlock->BlockSize;
-//             pInsertBlock->pNextBlock = pPrevBlock->pNextBlock->pNextBlock;
+    /*
+        Check if the previous block to the one being inserted make
+        a big block that is next to each other, if yes, then merge
+    */
+    if ((a_block *)((uint8_t *)pPrevBlock + pPrevBlock->BlockSize) == pInsertBlock)
+    {
+        pPrevBlock->BlockSize += pInsertBlock->BlockSize;
+        pInsertBlock = pPrevBlock;
+    }
 
-//         }
-//     }
-//     else
-//     {
-//         //set pointer accordingly
-//         pInsertBlock->pNextBlock = pPrevBlock->pNextBlock;
-//     }
+    /*
+        Same method as above but with next block.
+        If a previous merge was done, it doesnt modify
+        anything as we are dealing with pointers
+    */
+    if ((a_block *)((uint8_t *)pInsertBlock + pInsertBlock->BlockSize) == pPrevBlock->pNextBlock)
+    {
+        /*
+            If end was reached, then dont increase size.
+            Else, skip intermediate block
+        */
+        if (pPrevBlock->pNextBlock == heapEnd)
+        {
+            pInsertBlock->pNextBlock = heapEnd;
+        }
+        else
+        {
+            pInsertBlock->BlockSize += pPrevBlock->pNextBlock->BlockSize;
+            pInsertBlock->pNextBlock = pPrevBlock->pNextBlock->pNextBlock;
+        }
+    }
+    else
+    {
+        pInsertBlock->pNextBlock = pPrevBlock->pNextBlock;
+    }
 
-//     //if they didnt merge then set nextBlock from prevBlock accordingly
-//     if(pInsertBlock != pPrevBlock)
-//     {
-//         pPrevBlock->pNextBlock = pInsertBlock;
-//     }
-//     else
-//     {
-//         //we did something wrong
-//     }
-    
-// }
+    /*
+        If first merge was not done, set next block pointer
+        of previous block to the one being inserted
+    */
+    if (pInsertBlock != pPrevBlock)
+    {
+        pPrevBlock->pNextBlock = pInsertBlock;
+    }
+}
