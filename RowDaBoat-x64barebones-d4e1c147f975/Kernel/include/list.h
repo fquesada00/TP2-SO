93aba3673a4bc8fe98db05b1ed977a4672a77d19
GastonDonikian <gdonikian@itba.edu.ar>
2020-10-06 16:57:33 -0300

    Test lists

--- RowDaBoat-x64barebones-d4e1c147f975/Kernel/include/list.h
+++ RowDaBoat-x64barebones-d4e1c147f975/Kernel/include/list.h
@@ -4,6 +4,7 @@
 //Implementacion de listas de prioridad basada en https://www.geeksforgeeks.org/priority-queue-using-linked-list/
 typedef PCB elem_t;
 
+
 typedef struct ListElem { 
     elem_t data; 
   
@@ -29,17 +30,17 @@ void initList(Header * head,elem_t firstNode, int p, int ticket);
 
 listElem_t* newNode(elem_t d, int p, int tickets);
 
-elem_t peek(Header * head);
+elem_t peek(Header * head); //Esta anda bien
 
-void pop(Header * head);
+void pop(Header * head); //Este anda bien 
 
-void push(Header * head, elem_t d, int p, int tickets);
+void push(Header * head, elem_t d, int p, int tickets); //Este anda bien!
 
-int isEmpty(Header * head);
+int isEmpty(Header * head); //Esta anda bien
 
-elem_t next(Header * head);
+elem_t next(Header * head); //Este anda bien
 
-listElem_t remove(Header * head);
+listElem_t removeCurrent(Header * head);
 
 listElem_t removeElement(Header * head,elem_t elem);
 
