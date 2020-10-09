aa48a29073b7f188a53f94ce9697af830f120ac0
oserpe <oserpe@itba.edu.ar>
2020-10-08 22:37:46 -0300

    retomando pipes

--- RowDaBoat-x64barebones-d4e1c147f975/Kernel/IPC/pipes.c
+++ RowDaBoat-x64barebones-d4e1c147f975/Kernel/IPC/pipes.c
@@ -1,11 +1,19 @@
 #include <stdint.h>
 #include "../include/list.h"
 #include "../include/fds.h"
+#define MAX_PIPES 64
+typedef struct PIPE{
+    char buffer[BUFFER_SIZE];
+    size_t reading[PROCESSES+1];
+    size_t writing[PROCESSES+1];
+}pipe_struct;
 
+pipe_struct pipeArray[MAX_PIPES] = {0};
 
-int pipe(int fds[2]){
-    fds[0] = 0;
-    fds[1] = 0;
-    retrieveFds(getpid(),&fds[0],&fds[1]);
+int pipe(){
+    //creamos PCB con data que nos dan
+    //pipe(struct datos1, struct datos2, int fdLectura) -> inicializo todo desde kernel
+    //PCB ->  *pipe_struct lectura, *pipe_struct escritura
+    //pipe(void *entryPoint, int newFd, int oldFd)
     return -1;
 }
